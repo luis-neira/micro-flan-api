fcf6bff27f8df11b4d608b4a8e7d2e17e1f72a8b
Luis Neira <lneira41086@gmail.com>
2025-05-17 01:38:11 -0400

    add repo test

---
+++ test/repos/rentals/get-rentals.test.js
@@ -0,0 +1,20 @@
+const sinon = require("sinon");
+const { expect } = require("chai");
+const getRentalsByType = require("../../../src/repos/rental/get-rentals");
+
+describe("getRentals repo", () => {
+  it("should call db methods and return results (manual stubs)", async () => {
+    const db = {
+      select: sinon.stub().returnsThis(),
+      from: sinon.stub().returnsThis(),
+      where: sinon.stub().resolves([{ id: 1, property_type: "apartment" }]),
+    };
+
+    const result = await getRentalsByType({ db })("apartment");
+
+    expect(db.select.calledOnce).to.be.true;
+    expect(db.from.calledWith("rentals")).to.be.true;
+    expect(db.where.calledWith({ property_type: "apartment" })).to.be.true;
+    expect(result).to.deep.equal([{ id: 1, property_type: "apartment" }]);
+  });
+});
