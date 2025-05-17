83b9a2a366961309d8e3389c0a3648e28a298563
Luis Neira <lneira41086@gmail.com>
2025-05-17 01:12:32 -0400

    Add unit tests

--- test/controllers/rental/get-rentals.test.js
+++ test/controllers/rental/get-rentals.test.js
@@ -1,22 +1,26 @@
 const { expect } = require("chai");
 const sinon = require("sinon");
 const getRentals = require("../../../src/controllers/rental/get-rentals");
-const rentalRepo = require("../../../src/repos/rental");
 
-describe.skip("getRentals controller", () => {
-  it.skip("should return rentals from repo", async () => {
+describe("getRentals controller", () => {
+  it("should return rentals from repo", async () => {
     const fakeRentals = [{ id: 1 }, { id: 2 }];
-    sinon.stub(rentalRepo, "getRentals").resolves(fakeRentals);
 
-    const req = {};
+    const fake = sinon.fake.resolves(fakeRentals);
+
+    const mock = {
+      getRentals: fake,
+    };
+
+    const req = {
+      query: {},
+    };
     const res = {
       json: sinon.spy(),
     };
 
-    await getRentals({ rentalRepo })(req, res);
+    await getRentals({ rentalRepo: mock })(req, res);
 
     expect(res.json.calledWith(fakeRentals)).to.be.true;
-
-    rentalRepo.getRentals.restore();
   });
 });
