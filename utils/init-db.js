eac69c616beb9c659d17a861464032091bf5b5bc
Luis Neira <lneira41086@gmail.com>
2025-06-06 00:23:37 -0400

    Update init-db

--- utils/init-db.js
+++ utils/init-db.js
@@ -1,26 +1,14 @@
 "use strict";
 
-// const fs = require("node:fs");
-// const path = require("node:path");
-
 const knex = require("knex");
 const knexFile = require("../knexfile");
 const config = require("../config");
 
 const db = knex(knexFile[config.nodeEnv]);
 
-// const dbPath = path.resolve("rentals.db");
-
-// if (fs.existsSync(dbPath)) {
-//   fs.unlinkSync(dbPath);
-// }
-
 // Re-run knex with fresh migrations/seeds
 db.migrate
   .latest()
-  .then(() => {
-    return db.seed.run();
-  })
   .then(() => db.destroy())
   .then(() => {
     console.log("Data successfully imported to rentals.db");
