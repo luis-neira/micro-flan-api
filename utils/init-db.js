3b16372555aa44ce494bd9b4e876c7480d9c1782
Luis Neira <Luis.Neira@payscale.com>
2025-07-11 09:15:53 -0400

    update init-db

--- utils/init-db.js
+++ utils/init-db.js
@@ -1,20 +1,31 @@
 'use strict'
 
+const pino = require('pino')
 const knex = require('knex')
-const getKnexfile = require('../knexfile')
+
 const config = require('../src/config')
+const getKnexfile = require('../knexfile')
 
 const knexfile = getKnexfile(config)
 const db = knex(knexfile[config.dbEnv])
+
+let pinoOpts = {}
+if (config.dbEnv === "development") {
+  pinoOpts.transport = {
+    target: "pino-dev"
+  }
+}
+
+const logger = pino(pinoOpts)
 
 // Re-run knex with fresh migrations/seeds
 db.migrate
   .latest()
   .then(() => db.destroy())
   .then(() => {
-    console.log('Data successfully imported to rentals.db')
+    logger.info('Data successfully imported to rentals.db')
   })
   .catch((err) => {
-    console.error(err)
+    logger.error(err, "Migration error")
     process.exit(1)
   })
