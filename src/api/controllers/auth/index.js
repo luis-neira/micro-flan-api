d6979a89ecd272bebd5fc8df0950b73a1f153192
Luis Neira <Luis.Neira@payscale.com>
2025-07-16 01:29:38 -0400

    update auth and tenant controllers

--- src/api/controllers/auth/index.js
+++ src/api/controllers/auth/index.js
@@ -1,11 +1,14 @@
 'use strict'
 
+const wrapController = require('@lib/wrap-controller')
 const login = require('./login')
 
 function createAuthController () {
-  return {
+  const controller = {
     login: login()
   }
+
+  return wrapController(controller)
 }
 
 module.exports = createAuthController
--- src/api/controllers/tenant/index.js
+++ src/api/controllers/tenant/index.js
@@ -1,11 +1,14 @@
 'use strict'
 
+const wrapController = require('@lib/wrap-controller')
 const getTenants = require('./get-tenants')
 
 function createTenantsController (cradle) {
-  return {
+  const controller = {
     getTenants: getTenants(cradle)
   }
+
+  return wrapController(controller)
 }
 
 module.exports = createTenantsController
