d3238a5d54609b3b525d9e8853c24cc3e53b2fd2
Luis Neira <Luis.Neira@payscale.com>
2025-07-21 19:37:46 -0400

    Update validator

--- src/api/routes/rental.js
+++ src/api/routes/rental.js
@@ -5,16 +5,18 @@ const awilixExpress = require('awilix-express')
 
 const makeValidator = require('@middleware/validate')
 const makeRentalAPI = require('../controllers/rental')
-const { bodySchema } = require('../schemas/rental/create-rental')
-
-const { makeFunctionInvoker } = awilixExpress
+const createRentalSchema = require('../schemas/rental/create-rental')
 
 const router = express.Router()
+
+const { makeFunctionInvoker } = awilixExpress
 const api = makeFunctionInvoker(makeRentalAPI)
 
 const { validate } = makeValidator({ allErrors: true })
 
-router.route('/').get(api('getRentals')).post(validate({ body: bodySchema }), api('createRental'))
+router.route('/')
+  .get(api('getRentals'))
+  .post(validate({ body: createRentalSchema.bodySchema }), api('createRental'))
 
 router.route('/:id').patch(api('updateRental')).delete(api('deleteRental'))
 
