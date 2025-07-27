5c8dd0e85475da6e5bec050455ff059e3e399d3f
Luis Neira <Luis.Neira@payscale.com>
2025-07-27 04:30:22 -0400

    Update swagger docs

--- src/api/rental/index.js
+++ src/api/rental/index.js
@@ -72,6 +72,40 @@ const api = makeInvoker(cradle => ({
 /**
  * @swagger
  * definitions:
+ *   UpdateRental:
+ *     type: object
+ *     description: Must include at least one property to update
+ *     properties:
+ *       title:
+ *         type: string
+ *         example: Super Cool Flat
+ *       location:
+ *         type: string
+ *         example: New York, NY
+ *       price:
+ *         type: number
+ *         example: 2500
+ *       bedrooms:
+ *         type: integer
+ *         example: 1
+ *       bathrooms:
+ *         type: integer
+ *         example: 1
+ *       property_type:
+ *         type: string
+ *         example: apartment
+ *       description:
+ *         type: string
+ *         example: A beautiful and spacious studio apartment with high-end finishes. The building amenities include a gym, pool, and rooftop lounge.
+ *       image:
+ *         type: string
+ *         format: uri
+ *         example: https://images.unsplash.com/photo-1729053809347-c27ce1269bc3
+ */
+
+/**
+ * @swagger
+ * definitions:
  *   NewRental:
  *     allOf:
  *       - $ref: '#/definitions/RentalBase'
@@ -144,6 +178,43 @@ router.route('/')
   .post(validate({ body: createRentalSchema.bodySchema }), api('createRental'))
 
 router.route('/:id')
+  /**
+   * @swagger
+   * /rentals/{id}:
+   *   patch:
+   *     summary: Update a rental by ID
+   *     tags: [Rentals]
+   *     consumes:
+   *       - application/json
+   *     produces:
+   *       - application/json
+   *     parameters:
+   *       - name: id
+   *         in: path
+   *         required: true
+   *         type: integer
+   *         description: ID of the rental to update
+   *         example: 2
+   *       - in: body
+   *         name: rental
+   *         description: Fields to update (must include at least one)
+   *         required: true
+   *         schema:
+   *           $ref: '#/definitions/UpdateRental'
+   *     responses:
+   *       200:
+   *         description: Successfully updated rental
+   *         schema:
+   *           $ref: '#/definitions/Rental'
+   *       404:
+   *         description: Rental not found
+   *         schema:
+   *           type: object
+   *           properties:
+   *             message:
+   *               type: string
+   *               example: Rental not found
+   */
   .patch(validate({
     params: updateRentalSchema.paramsSchema,
     body: updateRentalSchema.bodySchema
